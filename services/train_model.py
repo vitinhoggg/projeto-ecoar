29ab6b87e474395a579a4415b2a65f2fd7061960
Vitao91 <victor78antonio.com@gmail.com>
2025-12-06 11:16:08 -0300

    Remove MongoDB URI for security reasons
    Removed sensitive MongoDB URI from the code.

--- services/train_model.py
+++ services/train_model.py
@@ -10,7 +10,7 @@ from sklearn.metrics import classification_report, accuracy_score
 import joblib
 
 load_dotenv()
-MONGO_URI = "mongodb+srv://vitinho:aluno123@ac-bmkbk7g.3znlu18.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+MONGO_URI = ""
 
 # Banco de Dados e Coleção:
 NOME_DO_BANCO = "ecoar"
